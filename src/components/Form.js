fa31b1c8ae76e4471ddda8829463dd4d8f319f56
Peyton Harrison <peyton006@hotmail.com>
2022-08-11 10:07:35 -0500

    commit tree 3ab0d538dd5e01d394facace183a82e8614d511d

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [people, setPeople] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,28 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  const reset = () => {
+    setFirstName("")
+    setLastName("")
+  }
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let newPerson = {firstName: firstName, lastName: lastName}
+  setPeople([...people, newPerson])
+  reset()
+}
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>People</h3>
+      {people.map(person => (<p>{person.firstName} {person.lastName}</p>))}
+    </div>
   );
 }
 
